099f201282310e3e419f1f737e4714aa2f796ceb
Taner Eyrice <taner.eyrice@gmail.com>
2019-02-23 15:12:35 +0000

    first call

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function index()
+    {
+        $keypair = new \Nexmo\Client\Credentials\Keypair(
+            file_get_contents(base_path('private.key')),
+            '8e2d8ffe-7ca6-4280-ac6a-d75f249264c5'
+        );
+
+        $client = new \Nexmo\Client($keypair);
+
+        $call = $client->calls()->create([
+            'to' => [[
+                'type' => 'phone',
+                'number' => '+447453533740'
+            ]],
+            'from' => [
+                'type' => 'phone',
+                'number' => '+447520632428'
+            ],
+            'answer_url' => ['https://developer.nexmo.com/ncco/tts.json'],
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/webhooks/events', 'EventController@index');
+Route::get('/webhooks/answer', 'AnswerController@index');
