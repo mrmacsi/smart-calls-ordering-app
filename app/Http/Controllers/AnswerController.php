57f1961fffcd3cdf5d4aa5311d6a3787b822b3b3
Taner Eyrice <taner.eyrice@gmail.com>
2019-02-23 17:30:12 +0000

    DTMF

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class AnswerController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $ncco = [
+            [
+                'action' => 'talk',
+                'text' => 'Please enter a digit'
+            ],
+            [
+                'action' => 'input',
+                'eventUrl' => [
+                    url('/webhooks/dtmf')
+                ]
+            ],
+            [
+                'action' => 'talk',
+                'text' => 'Thank you'
+            ],
+        ];
 
+        Log::info($ncco);
+        return response()->json($ncco);
     }
 }
